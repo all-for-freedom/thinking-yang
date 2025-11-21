469280eb53b8456c33c249736454510786c82493
all-for-freedom <yangshen@hunnu.edu.cn>
2025-11-21 08:07:03 +0800

    解决包更新以及引用问题

--- src/global.d.ts
+++ src/global.d.ts
@@ -18,6 +18,17 @@ declare global {
 
 		mobileTOCInit?: () => void;
 	}
+
+	interface Document {
+		startViewTransition?: (callback?: () => void | Promise<void>) => ViewTransition;
+	}
+
+	interface ViewTransition {
+		finished: Promise<void>;
+		ready: Promise<void>;
+		updateCallbackDone: Promise<void>;
+		skipTransition: () => void;
+	}
 }
 
 interface SearchResult {
